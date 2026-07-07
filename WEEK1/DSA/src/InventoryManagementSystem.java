28d6e3a2f893ab85adfa9c76f2b96ac0bb131750
vansh_gpt44 <156524605+vansh-gpt44@users.noreply.github.com>
2026-07-07 22:17:47 +0530

    Add files via upload

---
+++ WEEK1/DSA/src/InventoryManagementSystem.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+
+class Prod {
+    int productId;
+    String productName;
+    int quantity;
+    double price;
+
+    Prod(int productId, String productName, int quantity, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String toString() {
+        return productId + " " + productName + " Qty:" + quantity + " Price:" + price;
+    }
+}
+
+public class InventoryManagementSystem {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, Prod> inventory = new HashMap<>();
+
+        // Add Products
+        inventory.put(101, new Prod(101, "Laptop", 10, 50000));
+        inventory.put(102, new Prod(102, "Mouse", 25, 500));
+
+        // Display
+        System.out.println("Inventory:");
+        System.out.println(inventory);
+
+        inventory.put(101, new Prod(101, "Laptop", 15, 52000));
+        System.out.println("\nAfter Update:");
+        System.out.println(inventory);
+
+        // Delete Product
+        inventory.remove(102);
+        System.out.println("\nAfter Delete:");
+        System.out.println(inventory);
+    }
+}
