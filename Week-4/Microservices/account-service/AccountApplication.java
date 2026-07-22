bcea87ffa13b3100a913b552bc65d06164ed950e
vansh_gpt44 <156524605+vansh-gpt44@users.noreply.github.com>
2026-07-22 18:25:21 +0530

    Add files via upload

---
+++ Week-4/Microservices/account-service/AccountApplication.java
@@ -0,0 +1,10 @@
+
+package com.cognizant.account;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class,args);
+    }
+}
---
+++ Week-4/Microservices/eureka-server/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,10 @@
+
+package com.cognizant.eureka;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServerApplication.class,args);
+    }
+}
